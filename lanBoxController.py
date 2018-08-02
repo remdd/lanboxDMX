045be2be2442d936e5e55b646d456e9dc1378e3e
remdd <robin.ed.marsden@gmail.com>
2018-08-02 13:13:05 +0100

    Further implementation of snippets & rRaspberry Pi control

--- lanBoxController.py
+++ lanBoxController.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import socket, time, copy, datetime, sys, random
+import socket, time, copy, datetime, sys, random, threading
 
 #	change to true when running on Raspberry Pi
 runningOnPi = False
@@ -32,6 +32,7 @@ COMMANDS = {
 #	Controller settings
 playing = False
 timeStamp = 0
+userCommand = ''
 #	Min and max times of inactivity before next 'snippet' plays, in seconds
 snippetMinWait = 5
 snippetMaxWait = 10
@@ -83,10 +84,46 @@ def turnAllOff(s):
 	print "All lights turned off"
 
 
+def kbdListener():
+	global userCommand
+	userCommand = raw_input()
+	print "Input: " + userCommand
+
+def piListener():
+	global userCommand
+	while True:
+		btn_0_state = GPIO.input(conversationObjects[0].pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+		btn_1_state = GPIO.input(conversationObjects[1].pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+		btn_2_state = GPIO.input(conversationObjects[2].pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+		if btn_0_state == False:
+			print "Button 0 pressed"
+			playConversation(0)
+		elif btn_1_state == False:
+			print "Button 1 pressed"
+			playConversation(1)
+		elif btn_2_state == False:
+			print "Button 2 pressed"
+			playConversation(2)
+
+
+def startListenerThread():
+	if runningOnPi:
+		listener = threading.Thread(target=piListener)
+		listener.start()
+	else:
+		listener = threading.Thread(target=kbdListener)
+		listener.start()
+
+
 def launch():
 	###########	Code to run once on first load ##########
 	# Turn all lights off
 	sendCommand('off')
+	# Queue up a first snippet
+	queueNextSnippet()
+	#	Start input listener
+	startListenerThread()
+	global userCommand, listener
 
 	########### Main loop ##########
 	while 1:
@@ -100,36 +137,40 @@ def launch():
 
 		else:
 			if not playing:
-				queueNextSnippet()
-				tcflush(sys.stdin, TCIFLUSH)
-				command = raw_input("Enter command: ")
-				while not command:
-					print "Waiting"
+#				tcflush(sys.stdin, TCIFLUSH)
+				if userCommand:
+					print "Keyboard command! " + userCommand
+					sendCommand(userCommand)
+					userCommand = ''
+					startListenerThread()
+				else:
 					time.sleep(1)
-				sendCommand(command)
+					if datetime.datetime.now() > nextSnippetTime:
+						playSnippet(nextSnippetID)
 			else:
 				time.sleep(1)
 
 
 def queueNextSnippet():
+	global nextSnippetTime, nextSnippetID, lastSnippetID
 	print "Queueing snippet..."
 	wait = random.randint(snippetMinWait, snippetMaxWait)
 	nextSnippetTime = datetime.datetime.now() + datetime.timedelta(seconds=wait)
+	print nextSnippetTime
 	print "Next snippet in " + str(wait) + " seconds"
 	rand = random.randint(0, len(snippetObjects)-1)
 	print "Next snippet ID is " + str(rand)
-	snippet_next = rand
+	nextSnippetID = rand
 
 
-def unqueueSnippet():
-	print "Unqueuing snippet..."
+def playSnippet(snippetID):
+	print "Playing snippet " + str(snippetID)
+	lastSnippetID = snippetID
+	sendCommand('snippet', snippetID)
+	queueNextSnippet()
 
 
-def playSnippet(s, snippetID):
-	print snippetID
-
-
-def sendCommand(command):
+def sendCommand(command, snippetID=0):
 	sent = False
 	while not sent:
 		try:
@@ -142,26 +183,51 @@ def sendCommand(command):
 			if command.upper() in COMMANDS:
 				command = COMMANDS[command.upper()]
 				s.send(command)
+
 			elif command.upper() == 'TEST':
 				sendTestCommand(s)
+				queueNextSnippet()
+
 			elif command.upper() == 'TEST_LONG':
 				longTest(s)
+				queueNextSnippet()
+
 			elif command.upper() == 'OFF':
 				turnAllOff(s)
-			elif command.upper() == 'PLAY':
-				conversationID = raw_input("Enter conversation ID: ")
-				validID = False
-				for conversation in conversationObjects:
-					if conversation.id == int(conversationID):
-						validID = True
-						playConversation(s, int(conversationID))
-				if not validID:
-					print "No conversation found with ID " + conversationID
+
+			elif command.upper() == 'PLAY0':
+				playConversation(s, 0)
+
+			elif command.upper() == 'PLAY1':
+				playConversation(s, 1)
+
+			elif command.upper() == 'PLAY2':
+				playConversation(s, 2)
+
+			# elif command.upper() == 'PLAY':
+			# 	conversationID = raw_input("Enter conversation ID: ")
+			# 	validID = False
+			# 	for conversation in conversationObjects:
+			# 		if conversation.id == int(conversationID):
+			# 			validID = True
+			# 			playConversation(s, int(conversationID))
+			# 	if not validID:
+			# 		print "No conversation found with ID " + conversationID
+
+			elif command.upper() == 'SNIPPET':
+				for snippet in snippetObjects:
+					if snippetID == snippet.id:
+						command = '*C9' + DMX_UNIVERSE + snippet.channel + 'FF#'
+						print 'DMX command: ' + command
+						s.send(command)
+						time.sleep(snippet.duration)
+						command = '*C9' + DMX_UNIVERSE + snippet.channel + '00#'
+						s.send(command)
+
 			else:
 				s.send(command)
 
-			response = getResponse(s)
-			print response
+			getResponse(s)
 
 			# Close socket
 			sent = True
@@ -204,15 +270,14 @@ def playConversation(s, conversationID):
 		while len(triggerQueue) > 0:
 			time.sleep(0.1)
 			if datetime.datetime.now() - timeStamp > datetime.timedelta(seconds = triggerQueue[0].time):
-				print datetime.datetime.now()
 				print "Trigger! Time: " + str(triggerQueue[0].time)
 				command = getDMXCommand(triggerQueue[0].channel, triggerQueue[0].value)
 				sendCommand(command)
-				print "Command sent"
 				triggerQueue.pop(0)
 				print len(triggerQueue)
 
 		print("Conversation playback complete.")
+		queueNextSnippet()
 		playing = False
 
 	else:
